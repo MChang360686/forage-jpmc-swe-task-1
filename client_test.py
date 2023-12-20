af75c3f96820e7263a3e9c674961f933b3b7f674
MChang360686 <readingcyclist@gmail.com>
2023-12-19 23:37:11 -0500

    added unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       # Strip price from list of dataPoint variables
+       data_point = list(getDataPoint(quote))
+       assert(data_point[3]) == (quote['top_ask']['price'] + quote['top_bid']['price']) / 2
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,6 +21,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       # use getRatio
+       data_point = list(getDataPoint(quote))
+       assert(getRatio(data_point[1], data_point[2])) == data_point[1]/data_point[2]
 
 
   """ ------------ Add more unit tests ------------ """
